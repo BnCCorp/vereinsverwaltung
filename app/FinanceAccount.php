dacb3ebbb8a27587c01a386ccfaf2168157bc323
cabuze <lucascamp@web.de>
2018-02-22 19:09:10 +0100

    fix typo

--- app/FinanceAccount.php
+++ app/FinanceAccount.php
@@ -8,7 +8,7 @@ class FinanceAccount extends Model
 {
     protected $appends = ['amount'];
 
-    // TODO sum the transaction amounts wihcih match the account id
+    // TODO sum the transaction amounts which match the account id
     public function getAmountAttribute()
     {
         return $this->startamount * 10;
