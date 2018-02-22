86a2eaf2240d0edb0a72d64134fb9b96ab16c9f2
cabuze <lucascamp@web.de>
2018-02-22 18:42:22 +0100

    FinanceAccount add dummy amount

--- app/FinanceAccount.php
+++ app/FinanceAccount.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class FinanceAccount extends Model
 {
-    //
+    protected $appends = ['amount'];
+
+    // TODO sum the transaction amounts wihcih match the account id
+    public function getAmountAttribute()
+    {
+        return $this->startamount * 10;
+    }
 }
